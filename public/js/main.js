c12fac0bffcbed69a9a0b7f6da044ca4b0ed6d5e
MkMonir <khanmonir1335@gmail.com>
2022-03-29 21:54:28 +0600

    Emitting events from server to client and client to server

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,9 @@ const socket = io('/');
 socket.on('connect', () => {
   console.log('Successfully connected' + socket.id);
 });
+
+socket.on('client', (msg) => {
+  console.log(msg);
+
+  socket.emit('server', 'Hello Server');
+});
--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const path = require('path');
-const express = require('express');
 const http = require('http');
+const dotenv = require('dotenv');
+const express = require('express');
+const socketio = require('socket.io');
+
+dotenv.config({ path: './config.env' });
 
 const app = express();
 const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
 
-const PORT = 3000;
-const io = require('socket.io')(server);
+const io = socketio(server);
 
 io.on('connection', (socket) => {
   console.log(socket.id);
+
+  socket.on('server', (msg) => console.log(msg));
+
+  socket.emit('client', 'Hello Client');
 });
 
+const PORT = 5000 || process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
